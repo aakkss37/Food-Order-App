a8b9f48497a1099abc7bae5a536c073e20750b4b
amar kumar <amarkumar.sharma.124@gmail.com>
2022-08-31 12:26:44 +0530

    Restart the project from scratch

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import { useState } from "react";
-import Cart from "./components/Cart/Cart";
-import Header from "./components/Layout/Header";
-import AllMeals from "./components/Meals/AllMeals";
-import CartContextProvider from "./store/CartContectProvider";
-
-
-
 function App() {
-
-  const [cartIsShown, setCartShown] = useState(false)
-
-  const showCartHandler = ()=>{
-    setCartShown(true)
-  }
-
-  const hideCartHandler = ()=>{
-    setCartShown(false)
-  } 
-
   return (
-    // <Fragment>  ---> now no need of wraper component any more coz CartContextProvider component can be used as wraper.
-      <CartContextProvider>
-        {cartIsShown && <Cart onCloseCart = {hideCartHandler}/>}
-        <Header onShowCart = {showCartHandler} />
-        <main>
-          <AllMeals/>
-        </main>
-      </CartContextProvider>
-    // </Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
--- src/components/UI/Input.jsx
+++
@@ -1,9 +0,0 @@
-import css from "./Input.module.css"
-
-const Input = (props)=>{
-        return <div className={css.input}>
-                <label htmlFor={props.input.id}>{props.lable}</label>
-                <input type={props.type} {...props.input} />
-        </div>
-}
-export default Input
