f4ae4fb7a0752eca007ab159a9938d0bc62e634f
amar kumar <amarkumar.sharma.124@gmail.com>
2022-09-02 17:32:25 +0530

    Connected HeaderCartButton to the CartContext

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,14 +1,24 @@
+import { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+
+        const cartctx = useContext(CartContext)
+
+        const initialValue = 0
+        const numberOfCartItems = cartctx.items.reduce((currentValue, item) => {
+                return currentValue + item.amount
+         }, initialValue);
+
         return (
                 <button className={classes.button} onClick={props.onClick}>
                         <span className={classes.icon}>
                                 <CartIcon />
                         </span>
                         <span>Your Cart</span>
-                        <span className={classes.badge}>3</span>
+                        <span className={classes.badge}>{numberOfCartItems}</span>
                 </button>
         );
 };
